1e9b0741ff4db97f595b11f9058f7b86ced0e48a
nationalsportsacademyshooting-sketch <nationalsportsacademyshooting@gmail.com>
2026-08-05 02:14:57 +0530

    Improved login security

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,19 +46,19 @@ exports.login = async (req, res) => {
 
         const user = await User.findOne({ username });
 
-        if (!user) {
-            return res.status(400).json({
-                message: "User not found"
-            });
-        }
+if (!user) {
+    return res.status(400).json({
+        message: "Invalid username or password"
+    });
+}
 
         const isMatch = await bcrypt.compare(password, user.password);
 
-        if (!isMatch) {
-            return res.status(400).json({
-                message: "Wrong password"
-            });
-        }
+       if (!isMatch) {
+    return res.status(400).json({
+        message: "Invalid username or password"
+    });
+}
 
         const token = jwt.sign(
             {
