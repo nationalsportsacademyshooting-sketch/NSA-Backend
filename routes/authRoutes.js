6493fad8b01d2127b87c74fbbb7563bf3019b52a
nationalsportsacademyshooting-sketch <nationalsportsacademyshooting@gmail.com>
2026-08-05 21:30:54 +0530

    Added JWT authentication middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,11 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
+const adminMiddleware = require("../middleware/adminMiddleware");
 
 router.post("/register", authController.register);
 
 router.post("/login", authController.login);
 
-router.put("/change-admin", authController.changeAdmin);
+router.put(
+    "/change-admin",
+    authMiddleware,
+    adminMiddleware,
+    authController.changeAdmin
+);
 
 module.exports = router;
